d4c35af74c03df611c4a2d938a23a7efb4db21ea
Jeff <11721615+jeffellin@users.noreply.github.com>
2023-03-28 10:18:00 -0400

    add metadata token

--- clusters/appref-view/scripts/get-metadata-store-token.sh
+++ clusters/appref-view/scripts/get-metadata-store-token.sh
@@ -17,6 +17,6 @@ yq e -i '.clusters.view.metadatastore.app_ca = env(CA)' $PARAMS_YAML
 export AUTH_TOKEN=`kubectl get secret metadata-store-read-write-client -n metadata-store -ojsonpath="{.data.token}" | base64 -d`
 yq e -i '.clusters.view.metadatastore.auth_token = env(AUTH_TOKEN)' $PARAMS_YAML
 export SOPS_AGE_RECIPIENTS=$(yq eval '.AGE_RECIPIENTS' $PARAMS_YAML)
-sops -e --input-type yaml <(ytt -f meta-data-store-token-sensitive-values-template.yaml --data-values-file $PARAMS_YAML) > ../cluster-config/config/tap-install/meta-data-store-token-sensitive-values.sops.yaml
+sops -e --input-type yaml --output-type yaml <(ytt -f meta-data-store-token-sensitive-values-template.yaml --data-values-file $PARAMS_YAML) > ../cluster-config/config/tap-install/meta-data-store-token-sensitive-values.sops.yaml
 
 echo "commit new file to git to force update"
