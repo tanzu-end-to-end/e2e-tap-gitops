676f2c96d7f692b1f8c6201727a8c022b291a520
Jeff <11721615+jeffellin@users.noreply.github.com>
2023-04-01 15:42:23 -0400

    add workload

--- clusters/appref-view/scripts/get-metadata-store-token.sh
+++ clusters/appref-view/scripts/get-metadata-store-token.sh
@@ -11,17 +11,17 @@ CLUSTER_NAME=$1
 
 #kubectl config use-context $CLUSTER_NAME
 
-export CA=$(kubectl get secret -n metadata-store app-tls-cert -o json | jq -r ".data.\"ca.crt\"")
-yq e -i '.clusters.view.metadatastore.app_ca = env(CA)' $PARAMS_YAML
+#export CA=$(kubectl get secret -n metadata-store app-tls-cert -o json | jq -r ".data.\"ca.crt\"")
+#yq e -i '.clusters.view.metadatastore.app_ca = env(CA)' $PARAMS_YAML
 
-export AUTH_TOKEN=`kubectl get secret metadata-store-read-write-client -n metadata-store -ojsonpath="{.data.token}" | base64 -d`
-yq e -i '.clusters.view.metadatastore.auth_token = env(AUTH_TOKEN)' $PARAMS_YAML
+#export AUTH_TOKEN=`kubectl get secret metadata-store-read-write-client -n metadata-store -ojsonpath="{.data.token}" | base64 -d`
+#yq e -i '.clusters.view.metadatastore.auth_token = env(AUTH_TOKEN)' $PARAMS_YAML
 export SOPS_AGE_RECIPIENTS=$(yq eval '.AGE_RECIPIENTS' $PARAMS_YAML)
 #sops -e --input-type yaml --output-type yaml <(ytt -f meta-data-store-token-sensitive-values-template.yaml --data-values-file $PARAMS_YAML) > ../cluster-config/config/tap-install/meta-data-store-token-sensitive-values.sops.yaml
 
 echo "commit new file to git to force update"
 
-for f in ./templates/values/*.yaml;
+for f in ./templates/config/*.yaml;
 do 
     echo "Processing $f file...";
     newfile=$(basename $f)
