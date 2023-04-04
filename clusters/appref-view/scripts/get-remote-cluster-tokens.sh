77ca6b723c3a909efefa1c704e99692b4c009b10
Jeff <11721615+jeffellin@users.noreply.github.com>
2023-04-04 10:35:03 -0400

    add lax scan policy

--- clusters/appref-view/scripts/get-remote-cluster-tokens.sh
+++ clusters/appref-view/scripts/get-remote-cluster-tokens.sh
@@ -12,13 +12,13 @@ CLUSTER_NAME=$2
 kubectl config use-context $1
 
 
-export API_SERVER_URL=$(kubectl config view --minify --flatten -o jsonpath='{.clusters[0].cluster.server}')
-yq e -i '.clusters.tap_gui.'$CLUSTER_NAME'.api_server_url = env(API_SERVER_URL)' $PARAMS_YAML
+#export API_SERVER_URL=$(kubectl config view --minify --flatten -o jsonpath='{.clusters[0].cluster.server}')
+#yq e -i '.clusters.tap_gui.'$CLUSTER_NAME'.api_server_url = env(API_SERVER_URL)' $PARAMS_YAML
 
 export API_SERVER_CA=$(kubectl config view --minify --flatten -o jsonpath='{.clusters[0].cluster.certificate-authority-data}')
 yq e -i '.clusters.tap_gui.'$CLUSTER_NAME'.api_server_ca = env(API_SERVER_CA)' $PARAMS_YAML
 
-export SA_TOKEN=$(kubectl -n tap-gui get secret tap-gui-viewer-tapview -n tap-gui -o json| jq -r '.data["token"]' | base64 --decode)
+export SA_TOKEN=$(kubectl -n tap-gui get secret tap-gui-viewer -n tap-gui -o json| jq -r '.data["token"]' | base64 --decode)
 yq e -i '.clusters.tap_gui.'$CLUSTER_NAME'.sa_token = env(SA_TOKEN)' $PARAMS_YAML
 
 echo "View cluster config updated, now you have to apply updates"
