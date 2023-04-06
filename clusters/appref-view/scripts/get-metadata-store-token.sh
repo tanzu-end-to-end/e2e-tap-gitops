22d47440f2616d36bec7d492a963220bc5cf950c
Jeff <11721615+jeffellin@users.noreply.github.com>
2023-04-06 15:01:41 -0400

    update for metadatasotre and ready for appref2

--- clusters/appref-view/scripts/get-metadata-store-token.sh
+++ clusters/appref-view/scripts/get-metadata-store-token.sh
@@ -2,12 +2,11 @@
 
 : ${PARAMS_YAML?"Need to set PARAMS_YAML environment variable"}
 
-if [ ! $# -eq 2 ]; then
+if [ ! $# -eq 1 ]; then
   echo "Must cluster name as args"
   exit 1
 fi
 CONTEXT=$1
-CLUSTER_NAME=$2
 
 kubectl config use-context $1
 
@@ -15,8 +14,8 @@ export CA=$(kubectl get secret -n metadata-store app-tls-cert -o json | jq -r ".
 yq e -i '.clusters.metadatastore.app_ca = env(CA)' $PARAMS_YAML
 
 export AUTH_TOKEN=`kubectl get secret metadata-store-read-write-client -n metadata-store -ojsonpath="{.data.token}" | base64 -d`
-yq e -i '.clusters.view.metadatastore.auth_token = env(AUTH_TOKEN)' $PARAMS_YAML
+yq e -i '.clusters.metadatastore.auth_token = env(AUTH_TOKEN)' $PARAMS_YAML
 
-echo "use sops to update template"
+export SOPS_AGE_RECIPIENTS=$(yq eval '.AGE_RECIPIENTS' $PARAMS_YAML)
 
-sops -e --input-type yaml --output-type yaml <(ytt -f meta-data-store-token-sensitive-values-template.yaml  --data-values-file $PARAMS_YAML) > ../cluster-config/values/meta-data-store-token-sensitive-values-template.sops.yaml
+sops -e --input-type yaml --output-type yaml <(ytt -f templates/values/meta-data-store-token-sensitive-values-template.yaml  --data-values-file $PARAMS_YAML) > ../cluster-config/values/meta-data-store-token-sensitive-values-template.sops.yaml
