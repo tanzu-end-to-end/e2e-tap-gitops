ae6526562943bbe63803a0366744ff1297b4e03f
Chris DeLashmutt <cdelashmutt@vmware.com>
2023-06-30 15:45:19 -0400

    Add some values

--- clusters/e2e-acme/tanzu-sync/scripts/deploy.sh
+++ clusters/e2e-acme/tanzu-sync/scripts/deploy.sh
@@ -4,7 +4,6 @@ set -o errexit -o nounset -o pipefail
 
 kapp deploy -a tanzu-sync \
   -f <(ytt -f tanzu-sync/app/config \
-#           -f cluster-config/config/tap-install/.tanzu-managed/version.yaml \
            --data-values-file tanzu-sync/app/values/ \
            --data-values-file <(tanzu-sync/scripts/sensitive-values.sh) \
       ) $@
